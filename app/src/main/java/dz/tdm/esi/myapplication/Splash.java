efe8b31a571d9d25ace9a0ff00554b70e1608d78
youcef madji <youcef madji>
2017-05-27 17:48:47 +0100

    ajout geofence

--- app/src/main/java/dz/tdm/esi/myapplication/Splash.java
+++ app/src/main/java/dz/tdm/esi/myapplication/Splash.java
@@ -1,43 +1,184 @@
 package dz.tdm.esi.myapplication;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+import dz.tdm.esi.myapplication.Activities.FolderList;
 import dz.tdm.esi.myapplication.Activities.Login;
+import dz.tdm.esi.myapplication.Services.GeofenceTransitionsIntentService;
+import dz.tdm.esi.myapplication.Util.FireBaseDB;
 
-public class Splash extends AppCompatActivity {
-
+public class Splash extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    public static final long GEOFENCE_EXPIRATION_IN_HOURS = 12;
+    public static final long GEOFENCE_EXPIRATION_IN_MILLISECONDS = GEOFENCE_EXPIRATION_IN_HOURS * 60 * 60 * 1000;
+    public static final float GEOFENCE_RADIUS_IN_METERS = 1609;
+    private static final String TAG ="dz.tdm.esi.myapplication";
     private static int SPLASH_TIME_OUT = 3000;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private PendingIntent mGeofencePendingIntent;
 
+    ArrayList<Geofence> mGeofenceList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
+        mGeofenceList = new ArrayList<Geofence>();
+
+        buildGoogleApiClient();
+        populateGeofenceList();
+
+
+        // Kick off the request to build GoogleApiClient.
 
 
         new Handler().postDelayed(new Runnable() {
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
+
 
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(Splash.this, Login.class);
-                startActivity(i);
 
-                // close this activity
+                Intent i = new Intent(Splash.this, FolderList.class);
+                startActivity(i);
                 finish();
             }
         }, SPLASH_TIME_OUT);
 
+    }
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+    private void logSecurityException(SecurityException securityException) {
+        Log.e(TAG, "Invalid location permission. " +
+                "You need to use ACCESS_FINE_LOCATION with geofences", securityException);
+    }
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);// FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            Toast.makeText(this, String.valueOf(mLastLocation.getLatitude()+" "+String.valueOf(mLastLocation.getLongitude())), Toast.LENGTH_SHORT).show();
+           // mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            //mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+            Log.d(TAG, String.valueOf(mLastLocation.getLatitude()+" "+String.valueOf(mLastLocation.getLongitude())) );
+            SharedPreferences.Editor editor = getSharedPreferences("position", MODE_PRIVATE).edit();
+            editor.putString("latitude", String.valueOf(mLastLocation.getLatitude()));
+            editor.putString("longitude", String.valueOf(mLastLocation.getLongitude()));
+            editor.commit();
+        }
+        if (!mGoogleApiClient.isConnected()) {
+            Log.e("youcef","error");
+        }
+        try {
+            LocationServices.GeofencingApi.addGeofences(
+                    mGoogleApiClient,
+                    getGeofencingRequest(),
+                    getGeofencePendingIntent()
+            );
+        } catch (SecurityException securityException) {
+            logSecurityException(securityException);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+    public void populateGeofenceList() {
+
+        DatabaseReference myRef = FireBaseDB.getMyRef().child("Geofence");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+
+                mGeofenceList.clear();
+                long cpt = dataSnapshot.getChildrenCount();
+                for (int i = 1; i<= cpt; i++){
+                    DataSnapshot geofenceSnapshot = dataSnapshot.child(String.valueOf(i));
+                    mGeofenceList.add(new Geofence.Builder()
+                            .setRequestId(String.valueOf(geofenceSnapshot.child("key").getValue()))
+                            .setCircularRegion (Double.valueOf(geofenceSnapshot.child("latitude").getValue().toString()), Double.valueOf(geofenceSnapshot.child("longitude").getValue().toString()), GEOFENCE_RADIUS_IN_METERS)
+                            .setExpirationDuration(GEOFENCE_EXPIRATION_IN_MILLISECONDS)
+                            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                                    Geofence.GEOFENCE_TRANSITION_EXIT)
+                            .build());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(mGeofenceList);
+        return builder.build();
+    }
+    private PendingIntent getGeofencePendingIntent() {
+        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
