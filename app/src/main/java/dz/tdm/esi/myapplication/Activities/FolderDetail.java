cd95892e8a3d8e4082e1bf19873b36db75868534
moussab_amine <dm_amrine@esi.dz>
2017-05-27 12:39:50 +0100

    Detail Folders

--- app/src/main/java/dz/tdm/esi/myapplication/Activities/FolderDetail.java
+++ app/src/main/java/dz/tdm/esi/myapplication/Activities/FolderDetail.java
@@ -2,10 +2,14 @@ package dz.tdm.esi.myapplication.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import dz.tdm.esi.myapplication.R;
 
 public class FolderDetail extends AppCompatActivity {
+
+    TextView date, montant, nomAdver, nPermisAdvers, vehiculeAdvers, matriculeAdvers;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
